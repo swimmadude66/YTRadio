76b4b808d633b4d8e3f45fb835de9488a8541196
Adam Yost <swimmadude66@gmail.com>
2015-09-25 19:35:26 -0400

    more restructuring

---
+++ client/controllers/usercontroller.js
@@ -0,0 +1,29 @@
+app.controller('UserCtrl', function ($scope, $http) {
+  $scope.expand = false;
+  $scope.searchResults = [];
+  /*
+  * Client Methods
+  */
+  $scope.search=function(){
+    $http.get('/api/radio/search/'+$scope.searchCriteria)
+    .then(function(res){
+      var data = res.data;
+      if(data.Success){
+        $scope.searchResults = data.Videos;
+        $scope.expand=true;
+      }
+    });
+  }
+
+  $scope.addToQueue=function(vidinfo){
+    $http.post('/api/radio/queue', vidinfo)
+    .then(function(res){
+      var data = res.data;
+      if(data.Success){
+        console.log('Video Added');
+      }
+    });
+  };
+
+
+});
--- client/index.js
+++ client/index.js
@@ -3,16 +3,6 @@ var app = angular.module('YTRadio', ['youtube-embed', 'btford.socket-io']);
 app.controller('PageCtrl', function ($scope, $http) {
 
 
-  $scope.search=function(){
-    $http.get('/api/radio/search/'+$scope.searchCriteria)
-    .then(function(res){
-      var data = res.data;
-      if(data.Success){
-        console.log(data);
-        $scope.searchResults = data.Videos;
-      }
-    })
-  }
 });
 
 app.directive('theatre', function() {
@@ -20,5 +10,21 @@ app.directive('theatre', function() {
     restrict: 'E',
     controller: 'RadioCtrl',
 		templateUrl: 'views/theatre.html'
+	}
+});
+
+app.directive('radioControls', function() {
+  return {
+    restrict: 'E',
+    controller: 'RadioCtrl',
+		templateUrl: 'views/radiocontrols.html'
+	}
+});
+
+app.directive('userControls', function() {
+  return {
+    restrict: 'E',
+    controller: 'UserCtrl',
+		templateUrl: 'views/usercontrols.html'
 	}
 });
