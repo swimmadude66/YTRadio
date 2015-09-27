c1f0d85cd3e10150a93dd299d8bc098329e4867e
Adam Yost <swimmadude66@gmail.com>
2015-09-27 16:37:53 -0400

    format the user controls overlay

--- client/controllers/usercontroller.js
+++ client/controllers/usercontroller.js
@@ -1,10 +1,18 @@
 app.controller('UserCtrl', function ($scope, $http) {
   $scope.expand = false;
   $scope.searchResults = [];
+  $scope.isSearching = false;
+  $scope.playlists={
+    'Default':{
+      Name:"Default", Active:true, Contents:[]
+    }
+  };
+  $scope.playlist={Name:"Default", Active:true, Contents:[]};
   /*
   * Client Methods
   */
   $scope.search=function(){
+    $scope.isSearching=true;
     $http.get('/api/radio/search/'+$scope.searchCriteria)
     .then(function(res){
       var data = res.data;
@@ -12,6 +20,11 @@ app.controller('UserCtrl', function ($scope, $http) {
         $scope.searchResults = data.Videos;
       }
     });
+  }
+
+  $scope.viewPlaylist=function(name){
+    $scope.playlist=$scope.playlists[name];
+    $scope.isSearching = false;
   }
 
   $scope.addToQueue=function(vidinfo){
