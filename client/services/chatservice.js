c7510fd9c3919805965fd1fa765ccf8f1a5e0afb
Adam Coulon <adamgcoulon@gmail.com>
2015-09-30 00:50:56 -0500

    fixed double messages being received

--- client/services/chatservice.js
+++ client/services/chatservice.js
@@ -1,26 +1,11 @@
+app.factory('chatService', ['socketFactory', function(socketFactory){
+	
+	var chatIo = io.connect('http://127.0.0.1:3000/chat');
 
-app.factory('chatService', ['$rootScope', function($rootScope){
-	// setup chat connection and namespace
-	var chatConnection = io.connect('http://127.0.0.1:3000/chat');
+	var chatConnection = socketFactory({
+		ioSocket: chatIo
+	});
 
-	return {
-		on: function(event, callback){
-			chatConnection.on(event, function(){
-				var args = arguments;
-				$rootScope.$apply(function(){
-					callback.apply(chatConnection, args);
-				});
-			});
-		},
-		emit: function(event, data, callback) {
-			chatConnection.emit(event, data, function(){
-				var args = arguments;
-				$rootScope.$apply(function () {
-	  			if (callback) {
-	    			callback.apply(chatConnection, args);
-	  			}
-    		});
-			});
-		}
-	}
+	return chatConnection;
+
 }]);
