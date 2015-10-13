ac25dcc0b1d728911cf22a08a1e31659994fc15d
Adam Yost <swimmadude66@gmail.com>
2015-10-13 17:18:41 -0400

    fix playlists being lost.

--- client/controllers/usercontroller.js
+++ client/controllers/usercontroller.js
@@ -11,21 +11,33 @@ app.controller('UserCtrl', function ($scope, $http, ModalService, authService, m
   $scope.playlistName="Default";
   $scope.inQueue=false;
   $scope.queue =[];
-
-  $http.get('/api/playlists').then(function(res){
-    var data = res.data;
-    if(data.Success){
-      $scope.playlists = data.Playlists;
-      for(pl in data.Playlists){
-        if(pl.Active){
-          $scope.playlistName = pl.Name;
-          break;
-        }
-      }
-    }
-  });
   $scope.isAdding = false;
   $scope.userData;
+  fetch_playlist();
+
+  function fetch_playlist(){
+    if(!authService.getUser()){
+      return;
+    }
+    $http.get('/api/playlists').then(function(res){
+      var data = res.data;
+      console.log(data);
+      if(data.Success){
+        $scope.playlists = data.Playlists;
+        for(pl in data.Playlists){
+          if(pl.Active){
+            $scope.playlistName = pl.Name;
+            break;
+          }
+        }
+      }
+      else{
+        toastr.error(data.Error, 'Problem retrieving playlist');
+      }
+    }, function(err){
+      toastr.error(err, 'Problem retrieving playlist');
+    });
+  }
 
   function addToQueue(callback){
     if($scope.playlists[$scope.playlistName].Contents.length <1){
@@ -133,7 +145,7 @@ app.controller('UserCtrl', function ($scope, $http, ModalService, authService, m
     $scope.playlists[$scope.playlistName].Contents.splice(ind, 1);
     $http.post('/api/playlists/update', $scope.playlists[$scope.playlistName]).then(function(res){
       var data = res.data;
-      if(data.Success){     
+      if(data.Success){
       }
       else{
         console.log(data.Error);
@@ -186,7 +198,9 @@ app.controller('UserCtrl', function ($scope, $http, ModalService, authService, m
       controller: "AuthCtrl"
     }).then(function(modal) {
       modal.element.modal();
-      modal.close.then(function(result) {});
+      modal.close.then(function(result) {
+        fetch_playlist();
+      });
     });
   };
 
