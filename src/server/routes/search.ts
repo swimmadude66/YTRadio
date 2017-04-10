1972faa98d522aa4320f803c6a74ba3c8c63725b
Adam Yost <swimmadude66@users.noreply.github.com>
2017-04-10 15:53:20 -0400

    undo replace here, do it before save

--- src/server/routes/search.ts
+++ src/server/routes/search.ts
@@ -27,8 +27,8 @@ module.exports = (APP_CONFIG) => {
                 }
                 let body = {
                     ID: result.id.videoId,
-                    Title: result.snippet.title.replace(/[\u0800-\uFFFF]/g, ''),
-                    Poster: result.snippet.channelTitle.replace(/[\u0800-\uFFFF]/g, ''),
+                    Title: result.snippet.title,
+                    Poster: result.snippet.channelTitle,
                     Thumbnails: result.snippet.thumbnails
                 };
                 cleanvids[body.ID] = body;
