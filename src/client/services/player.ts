3d491d408bbdf017e1bd3d9e67778ef8f2f8c443
Adam Yost <swimmadude66@gmail.com>
2017-11-02 19:46:09 -0400

    angular upgrae and greentext bug fix

--- src/client/services/player.ts
+++ src/client/services/player.ts
@@ -63,7 +63,7 @@ export class PlayerService {
     }
 
     onPlayerStateChange(event, that) {
-        if (event.data === 2) {
+        if (!this.novid && event.data === 2) {
             that.player.playVideo();
         } else {
             that.playerEvents.next({Event: 'stateChange', Data: event.data});
@@ -73,19 +73,16 @@ export class PlayerService {
     playVideo(video, startSeconds) {
         if (video) {
             this.novid = false;
-            if (this.player && this.player.clearVideo && this.player.playVideo) {
-                this.player.clearVideo();
-                this.player.cueVideoById(video.Info.ID, startSeconds || 0);
+            if (this.player && this.player.playVideo) {
                 this.playerInfo.start = startSeconds || 0;
-                this.player.playVideo();
+                this.player.loadVideoById(video.Info.ID, startSeconds || 0);
             } else {
                 this.playerInfo.start = startSeconds || 0;
                 this.playerInfo.VideoId = video.Info.ID;
             }
         } else {
             this.novid = true;
-            this.playerInfo = {};
-            if (this.player && this.player.stopVideo) {
+            if (this.player) {
                 this.player.stopVideo();
             }
         }
